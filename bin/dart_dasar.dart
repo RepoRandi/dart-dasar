52757d4338056b60eb628d6667ade92703323929
Randi <randimaulanaakbar@gmail.com>
2022-12-11 22:42:10 +0700

    Initial commit

---
+++ bin/dart_dasar.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dasar/dart_dasar.dart' as dart_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dasar.calculate()}!');
+}
---
+++ test/dart_dasar_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dasar/dart_dasar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
