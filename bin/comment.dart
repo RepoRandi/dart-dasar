602ca10d120311b0735e3c3b48102417f917dc44
Randi <randimaulanaakbar@gmail.com>
2022-12-18 18:33:17 +0700

    materi komentar

---
+++ bin/comment.dart
@@ -0,0 +1,14 @@
+/// this is main function
+/// will executed by dart
+void main() {
+
+  // this is name variable
+  var name = 'Randi Maulana Akbar';
+
+  /**
+   * this is multi line comment
+   * you can add more here
+   */
+
+  print(name);
+}
