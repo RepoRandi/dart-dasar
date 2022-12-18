7b7acd888ddf75373b65d3b97392870db60e72ce
Randi <randimaulanaakbar@gmail.com>
2022-12-18 18:42:27 +0700

    materi number

---
+++ bin/number.dart
@@ -0,0 +1,15 @@
+void main() {
+  int number1 = 1;
+  double number2 = 2.2;
+
+  print(number1);
+  print(number2);
+
+  num number = 10;
+
+  print(number);
+
+  number = 10.5;
+
+  print(number);
+}
