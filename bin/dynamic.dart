cfa59683c0a12e1351e27e87af04a81e29d1e46f
Randi <randimaulanaakbar@gmail.com>
2023-01-16 18:10:31 +0700

    materi dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,13 @@
+void main() {
+  dynamic variable = 1000;
+  print(variable);
+
+  variable = 'Seribu';
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = DateTime.now();
+  print(variable);
+}
