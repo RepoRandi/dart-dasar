bccad6534212f4caf0ddda6f07a95f3670f678e2
Randi <randimaulanaakbar@gmail.com>
2023-01-17 08:52:30 +0700

    materi operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,15 @@
+void main() {
+  var name1 = 'randi';
+  var name2 = 'fera';
+  var number1 = 10;
+  var number2 = 5;
+
+  print(name1 == name2);
+  print(name1 != name2);
+  print(number1 == number2);
+  print(number1 != number2);
+  print(number1 > number2);
+  print(number1 >= number2);
+  print(number1 < number2);
+  print(number1 <= number2);
+}
