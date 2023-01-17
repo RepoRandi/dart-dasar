f319600bd6f3e7e0a16a785e2d609eafa9e4ec74
Randi <randimaulanaakbar@gmail.com>
2023-01-17 08:43:39 +0700

    materi operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,19 @@
+void main() {
+  var res1 = 10 + 10;
+  var res2 = 100 - 10;
+  var res3 = -5;
+  var res4 = 5;
+  var res5 = 5 * 5;
+  var res6 = 10 / 3;
+  var res7 = 10 ~/ 3;
+  var res8 = 10 % 3;
+
+  print(res1);
+  print(res2);
+  print(res3);
+  print(res4);
+  print(res5);
+  print(res6);
+  print(res7);
+  print(res8);
+}
