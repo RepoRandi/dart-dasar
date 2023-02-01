e5843a7d86f33dccfc2cc7d4e325b98326708db9
Randi <randimaulanaakbar@gmail.com>
2023-02-01 21:51:08 +0700

    materi for

---
+++ bin/for.dart
@@ -0,0 +1,16 @@
+void main() {
+  for (;;) {
+    print('Perulanagan tidak berhenti-henti');
+  }
+
+  int counter = 1;
+
+  for (; counter <= 10;) {
+    print('Perulanagan ke - $counter');
+    counter++;
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    print('Perulangan ke - $i');
+  }
+}
